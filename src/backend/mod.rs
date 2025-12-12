7fe02f415d2c35a68f211405ccf507f9216f621c
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-12 22:09:33 +0000

    refactor:  complete backend entity move

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,5 +1,11 @@
 mod inmemory;
 mod graph;
+mod storednode;
+mod storedrel;
+mod graphstore;
 
-pub use inmemory::InMemoryBackend;
+pub use inmemory::{InMemoryBackend};
+pub use storednode::StoredNode;
+pub use storedrel::StoredRel;
+pub use graphstore::GraphStore;
 pub use graph::*;
