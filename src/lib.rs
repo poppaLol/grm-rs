368df1d090aaace2a15ce27e1f5fd2bf0a19e66e
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-14 16:43:56 +0000

    feat: expose further types

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ pub mod macros;
 pub mod dsl;
 
 // Re-exports for convenient use
-pub use backend::{GraphBackend, GraphTx, QueryResult};
+pub use backend::{GraphBackend, GraphTx, QueryResult, StoredNode, StoredRel, InMemoryBackend};
 pub use model::{NodeModel, RelModel};
-pub use error::GrmError;
+pub use error::{GrmError, Result};
 pub use repo::{NodeRepository, RelRepository};
 pub use grm_rs_macros::*;
 pub use dsl::{NodePattern, Property, PropertyFilter, CompareOp, Query, QueryKind, VarGen};
