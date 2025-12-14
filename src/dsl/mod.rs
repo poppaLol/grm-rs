101fca5e6bfa7d7f4b3b16a04a72e147acccf6ec
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-14 22:40:15 +0000

    export funcs and structs from dsl module

--- src/dsl/mod.rs
+++ src/dsl/mod.rs
@@ -5,13 +5,15 @@ mod nodepattern;
 mod graph;
 mod eval;
 mod paging;
+mod result;
 
 pub use query::{Query, QueryKind};
 pub use compare::CompareOp;
 pub use property::{Property, PropertyFilter};
 pub use nodepattern::NodePattern;
 pub use graph::{Direction, GraphQuery, MatchClause, Return, VarId,
-    HopMatch, NodeMatch, VarGen
+    HopMatch, NodeMatch, VarGen, var_key
 };
 pub use eval::{numeric_cmp, props_match_filters};
 pub use paging::apply_paging;
+pub use result::{QueryResult,QueryRow};
