67450702b3fa596d6d37ec619b680d7e95ab605c
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-20 14:08:29 +0000

    feat: re-export client and decode as ergonomic layer

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,18 @@ pub mod error;
 pub mod macros;
 pub mod model;
 pub mod repo;
+pub mod client;
+pub mod decode;
 
 // Re-exports for convenient use
 pub use backend::{GraphBackend, GraphTx, InMemoryBackend, StoredNode, StoredRel};
 pub use dsl::{
     CompareOp, GraphQuery, NodePattern, Property, PropertyFilter, Props, Query, QueryKind,
-    QueryResult, VarGen,
+    QueryResult, VarGen, QueryRow
 };
 pub use error::{GrmError, Result};
 pub use grm_rs_macros::*;
 pub use model::{NodeModel, RelModel};
 pub use repo::{NodeRepository, RelRepository};
+pub use client::GraphClient;
+pub use decode::DecodeFromRow;
