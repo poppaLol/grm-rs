7eb435276ce85a5c7f6276873ad880a79eea910e
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-26 17:46:00 +0000

    feat: add autocommit transaction wrapper

---
+++ src/repo/macros.rs
@@ -0,0 +1,26 @@
+// src/macros.rs
+#[macro_export]
+macro_rules! autocommit {
+    ($backend:expr, |$tx:ident| $body:expr) => {{
+        // 1) Begin backend tx (raw)
+        let __inner = $backend.begin_tx().await?;
+
+        // 2) Wrap it in the public Transaction wrapper
+        let mut $tx = $crate::client::Transaction::from_inner(__inner);
+
+        // 3) Run body (must evaluate to Result<T>)
+        let __out = $body;
+
+        // 4) Commit or rollback once
+        match __out {
+            Ok(__v) => {
+                $tx.commit().await?;
+                Ok(__v)
+            }
+            Err(__e) => {
+                let _ = $tx.rollback().await;
+                Err(__e)
+            }
+        }
+    }};
+}
