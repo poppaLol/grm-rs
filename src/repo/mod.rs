2348d4bb677f35cb749eff3393e926d09e4d41b7
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-26 17:49:09 +0000

    feat: transaction oriented repos as main path

--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -1,8 +1,8 @@
 mod node_repo;
 mod rel_repo;
-mod labels;
 mod repo;
+mod macros;
 
-pub use node_repo::NodeRepository;
-pub use rel_repo::RelRepository;
+pub use node_repo::{NodeRepository, NodeRepositoryTx};
+pub use rel_repo::{RelRepository, RelRepositoryTx};
 pub use repo::Repo;
