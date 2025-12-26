b638bbb7811ace1b03d62318f7a2dc695d97ba82
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-26 17:45:27 +0000

    refactor: move labels_match

--- src/repo/labels.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) fn labels_match(
-    labels: &[String],
-    expected: &'static [&'static str],
-) -> bool {
-    expected
-        .iter()
-        .all(|l| labels.iter().any(|sl| sl == l))
-}
