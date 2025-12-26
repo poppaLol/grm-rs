99e18e1ba19b4a3c2bcc0bea9e5c04f3579fdc2b
Laurie Ibbs <laurie.ibbs@gmail.com>
2025-12-26 17:45:27 +0000

    refactor: move labels_match

---
+++ src/decode/labels.rs
@@ -0,0 +1,7 @@
+use crate::{NodeModel, StoredNode};
+
+pub fn labels_match<M: NodeModel>(n: &StoredNode) -> bool {
+    M::LABELS
+        .iter()
+        .all(|l| n.labels.iter().any(|sl| sl == l))
+}
--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -1,6 +1,8 @@
 mod decoderow;
 mod node;
 mod rel;
+mod labels;
 
 pub use decoderow::DecodeFromRow;
 pub use rel::decode_rel_from_row;
+pub use labels::labels_match;
