01e6a579e4169818170f921f1829ccfd9af55a9c
Laurie Ibbs <laurie.ibbs@gmail.com>
2026-04-22 08:04:37 +0000

    refactor: update relationship repository flow

--- src/repo/rel_repo.rs
+++ src/repo/rel_repo.rs
@@ -4,7 +4,7 @@ use std::marker::PhantomData;
 use serde_json::Value as JsonValue;
 
 use crate::{
-    autocommit,
+    autocommit, autoread,
     backend::{GraphBackend, GraphTx},
     client::Transaction,
     error::Result,
@@ -46,13 +46,21 @@ where
         })
     }
 
+    /// Create a relationship between two nodes by i64 IDs.
+    pub async fn create_between_i64(&self, from_id: i64, to_id: i64, rel: &mut R) -> Result<()> {
+        autocommit!(self.backend, |tx| {
+            let mut repo_tx = RelRepositoryTx::<B::Tx, R>::new(&mut tx);
+            repo_tx.create_between_i64(from_id, to_id, rel).await
+        })
+    }
+
     /// Get all outgoing relationships of this type from a given node,
-    /// returning (relationship, target_node) pairs. (typed)
+    /// returning (relationship, target_node_id) pairs. (typed)
     pub async fn outgoing_from(
         &self,
         from_id: &<R::From as NodeModel>::Id,
-    ) -> Result<Vec<(R, R::To)>> {
-        autocommit!(self.backend, |tx| {
+    ) -> Result<Vec<(R, Option<i64>)>> {
+        autoread!(self.backend, |tx| {
             let mut repo_tx = RelRepositoryTx::<B::Tx, R>::new(&mut tx);
             repo_tx.outgoing_from(from_id).await
         })
@@ -61,8 +69,8 @@ where
     pub async fn incoming_to(
         &self,
         to_id: &<R::To as NodeModel>::Id,
-    ) -> Result<Vec<(R, R::From)>> {
-        autocommit!(self.backend, |tx| {
+    ) -> Result<Vec<(R, Option<i64>)>> {
+        autoread!(self.backend, |tx| {
             let mut repo_tx = RelRepositoryTx::<B::Tx, R>::new(&mut tx);
             repo_tx.incoming_to(to_id).await
         })
@@ -107,13 +115,35 @@ where
             .await?;
 
         rel.set_id(stored.id.into());
+        rel.set_from(from_raw);
+        rel.set_to(to_raw);
+        Ok(())
+    }
+
+    pub async fn create_between_i64(
+        &mut self,
+        from_id: i64,
+        to_id: i64,
+        rel: &mut R,
+    ) -> Result<()> {
+        let props: BTreeMap<String, JsonValue> = rel.to_properties();
+
+        let stored = self
+            .tx
+            .tx_mut()?
+            .create_relationship(from_id, to_id, R::TYPE, props)
+            .await?;
+
+        rel.set_id(stored.id.into());
+        rel.set_from(from_id);
+        rel.set_to(to_id);
         Ok(())
     }
 
     pub async fn outgoing_from(
         &mut self,
         from_id: &<R::From as NodeModel>::Id,
-    ) -> Result<Vec<(R, R::To)>> {
+    ) -> Result<Vec<(R, Option<i64>)>> {
         let from_raw: i64 = from_id.clone().into();
 
         let pairs = self.tx.tx_mut()?.outgoing(from_raw, Some(R::TYPE)).await?;
@@ -123,27 +153,29 @@ where
         for (stored_rel, stored_node) in pairs {
             // Decode relationship model
             let rel_id: R::Id = stored_rel.id.into();
-            let rel_model = R::from_properties(rel_id, stored_rel.props)?;
+            let rel_model = R::from_parts(
+                rel_id,
+                stored_rel.from.into(),
+                stored_rel.to.into(),
+                stored_rel.props,
+            )?;
 
             // Optional: enforce target node labels match R::To
             if !labels_match::<R::To>(&stored_node) {
                 continue;
             }
 
-            // Decode target node model
-            let node_id: <R::To as NodeModel>::Id = stored_node.id.into();
-            let node_model = <R::To as NodeModel>::from_properties(node_id, stored_node.props)?;
-
-            out.push((rel_model, node_model));
+            // Return only the endpoint ID, not the full node model
+            out.push((rel_model, Some(stored_node.id.into())));
         }
 
         Ok(out)
     }
 
-        pub async fn incoming_to(
+    pub async fn incoming_to(
         &mut self,
         to_id: &<R::To as NodeModel>::Id,
-    ) -> Result<Vec<(R, R::From)>> {
+    ) -> Result<Vec<(R, Option<i64>)>> {
         let to_raw: i64 = to_id.clone().into();
 
         let pairs = self.tx.tx_mut()?.incoming(to_raw, Some(R::TYPE)).await?;
@@ -153,19 +185,15 @@ where
         for (stored_rel, stored_node) in pairs {
             // Decode relationship model
             let rel_id: R::Id = stored_rel.id.into();
-            let rel_model = R::from_properties(rel_id, stored_rel.props)?;
+            let rel_model = R::from_parts(rel_id, stored_rel.from.into(), stored_rel.to.into(), stored_rel.props)?;
 
             // Enforce node labels match R::From (because we're decoding R::From from stored_node)
             if !labels_match::<R::From>(&stored_node) {
                 continue;
             }
 
-            // Decode source node model
-            let node_id: <R::From as NodeModel>::Id = stored_node.id.into();
-            let node_model =
-                <R::From as NodeModel>::from_properties(node_id, stored_node.props)?;
-
-            out.push((rel_model, node_model));
+            // Return only the endpoint ID, not the full node model
+            out.push((rel_model, Some(stored_node.id.into())));
         }
 
         Ok(out)
