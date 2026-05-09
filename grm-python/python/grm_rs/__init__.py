729a5dc255fe88454c7e841f529b5f67f99241fb
Laurie Ibbs <laurie.ibbs@gmail.com>
2026-05-09 09:42:28 +0000

    Expose Neo4j sessions in Python bindings

--- grm-python/python/grm_rs/__init__.py
+++ grm-python/python/grm_rs/__init__.py
@@ -1,3 +1,5 @@
-from ._grm_rs import GrmError, Session
+from ._grm_rs import GrmError, Neo4jSession, Session
+from .neo4j import AsyncNeo4jSession
 
-__all__ = ["GrmError", "Session"]
+
+__all__ = ["AsyncNeo4jSession", "GrmError", "Neo4jSession", "Session"]
