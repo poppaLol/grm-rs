3edca2313fb023d0cc7eaa0654923752a75538bd
Laurie Ibbs <laurie.ibbs@gmail.com>
2026-05-12 12:56:39 +0000

    Add shared runtime batch operations

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,7 +1,15 @@
+mod batch;
 mod catalog;
 mod parser;
 mod session;
 
+pub use batch::{
+    SessionBatchDefineEdgeParams, SessionBatchDefineNodeParams, SessionBatchEdgeCreateParams,
+    SessionBatchEdgeDeleteParams, SessionBatchEdgeUpdateParams, SessionBatchEndpoint,
+    SessionBatchFieldParam, SessionBatchNodeCreateParams, SessionBatchNodeDeleteParams,
+    SessionBatchNodeUpdateParams, SessionBatchOp, SessionBatchOutcome, SessionBatchParams,
+    SessionBatchResponse, apply_session_batch,
+};
 pub use catalog::{
     RuntimeField, RuntimeNodeModel, RuntimeRelModel, RuntimeValueType, SessionModelCatalog,
     parse_required_flag, validate_field_name, validate_model_name,
